d2a7a966ea7c84d120be747bec485c27f4217ab1
Jennifer Oh <ohjennifer.90@gmail.com>
2019-04-23 17:55:30 -0400

    school-domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :school_name, :roster
+
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = { }
+  end
+
+
+  def add_student(name, grade)
+    if roster[grade]
+      roster[grade] << name
+    else roster[grade] = [ ]
+      roster[grade] << name
+
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+    binding.pry
+  end
+
+  def sort
+    roster.each do |grade, name|
+      roster[grade] = name.sort
+    end
+  end
+
+end
