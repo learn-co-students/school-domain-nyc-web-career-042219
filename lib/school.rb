ad56019a15b9997f36af90cf8577b942ce9b6863
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-24 13:52:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -4,5 +4,15 @@ class School
      @name = name
      @roster = {}
    end
-
+  def add_student(name, grade)
+    @name = name 
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+    
+    # roster[grade] = []
+    # roster[grade] << name
+  end
 end
