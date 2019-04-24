79fb90d3f2cb30af79a263e38694d5632bcc8b06
Kane Koo <kanekoo@gmail.com>
2019-04-23 21:12:54 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name=name
+    @roster={}
+
+  end
+
+  def roster
+    @roster
+  end
+  def add_student(student,grade)
+
+    flag=@roster.keys.include?(grade)
+
+    if flag
+        @roster[grade] << student
+    else
+        @roster[grade]=[]
+        @roster[grade] << student
+      end
+
+    #    binding.pry
+  #
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.map do |grade, array|
+      @roster[grade] = array.sort!
+    end
+    @roster
+  end 
+end
