e7fe9db02547c8540aa237da031fe06f6328db8f
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-04-23 21:28:34 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :roster
+
+  def initialize(roster)
+    @roster = roster
+    @roster = {}
+  end
+
+    def add_student(name, num)
+
+      if !@roster[num]
+        @roster[num] = []
+      end
+      @roster[num].push(name)
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each {|grade,names| names.sort! }
+    end
+  end
